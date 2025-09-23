25062d8a37323cfa1038f7f2dd3c9aa319c0dd9c
pozercpp <n.a.chigrin@yandex.ru>
2025-09-24 00:38:17 +0300

    Update decimal.h

--- include/decimal.h
+++ include/decimal.h
@@ -1,28 +1,27 @@
 #pragma once
 
 #include <initializer_list>
-#include <myvector.h>
 #include <iostream>
+#include <myvector.h>
 
 class Decimal {
 public:
-
   Decimal();
   Decimal(std::initializer_list<unsigned char>);
   Decimal(unsigned long long);
   Decimal(unsigned int);
 
   ~Decimal();
-  Decimal(const Decimal&);
-  Decimal(Decimal&&) noexcept;
+  Decimal(const Decimal &);
+  Decimal(Decimal &&) noexcept;
 
   unsigned char get(size_t);
-  
+
   int len();
 
   void print();
-  
-  myvector v;
+
+  MyVector v;
 };
 
 Decimal plus(Decimal, Decimal);
