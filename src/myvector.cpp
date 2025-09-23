e4ca59a0180b035a759fa727a3f3bb4c17a96fc1
pozercpp <n.a.chigrin@yandex.ru>
2025-09-24 00:40:25 +0300

    Update myvector.cpp

--- src/myvector.cpp
+++ src/myvector.cpp
@@ -1,59 +1,49 @@
 #include "myvector.h"
 
-myvector::myvector() : size(0), capacity(1), v(nullptr) {
-    v = (unsigned char *)malloc(sizeof(unsigned char) * capacity);
+MyVector::MyVector() : size(0), capacity(1), v(new unsigned char[capacity]) {}
+
+MyVector::MyVector(const MyVector &other)
+    : size(other.size), capacity(other.capacity),
+      v(new unsigned char[capacity]) {
+  std::copy(other.v, other.v + size, v);
 }
 
-myvector::myvector(const myvector& other) : size(0), capacity(1), v(nullptr) {
-    v = (unsigned char *)malloc(sizeof(unsigned char) * capacity);
-    for (size_t i = 0; i < other.size; ++i) {
-        push(other.get(i));
-    }
+MyVector::~MyVector() { delete[] v; }
+
+void MyVector::resize(size_t newsize) {
+  capacity = newsize;
+  unsigned char *t = new unsigned char[capacity];
+  std::copy(v, v + size, t);
+  delete[] v;
+  v = t;
 }
 
-myvector::~myvector() {
-    if (v != nullptr) {
-        v = nullptr;
-    }
-    size = 0;
-    capacity = 1;
-    v = (unsigned char *)malloc(sizeof(unsigned char) * capacity);
+void MyVector::push(unsigned char c) {
+  if (size >= capacity) {
+    resize(2 * capacity);
+  }
+  v[size++] = c;
 }
 
-void myvector::resize(size_t newsize) {
-    capacity = newsize;
-    v = (unsigned char *)realloc(v, sizeof(unsigned char) * capacity);
+void MyVector::pop() {
+  if (size) {
+    --size;
+  }
 }
 
-void myvector::push(unsigned char c) {
-    if (size >= capacity) {
-        resize(2 * capacity);
-    }
-    v[size++] = c;
+unsigned char MyVector::get(size_t i) const { return v[i]; }
+
+size_t MyVector::len() const { return size; }
+
+void MyVector::swap(size_t i, size_t j) {
+  unsigned char c = v[i];
+  v[i] = v[j];
+  v[j] = c;
 }
 
-void myvector::pop() {
-    if (size) {
-        --size;
-    }
-}
-
-unsigned char myvector::get(size_t i) const {
-    return v[i];
-}
-
-size_t myvector::len() {
-    return size;
-}
-
-void myvector::swap(size_t i, size_t j) {
-    unsigned char c = v[i];
-    v[i] = v[j];
-    v[j] = c;   
-}
-
-void myvector::clear() {
-    size = 0;
-    capacity = 1;
-    v = (unsigned char *)malloc(sizeof(unsigned char) * capacity);
+void MyVector::clear() {
+  size = 0;
+  capacity = 1;
+  delete[] v;
+  v = new unsigned char[capacity];
 }
